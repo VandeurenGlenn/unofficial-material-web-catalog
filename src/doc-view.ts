50281cf98d68c063146d6f47f96b64122c843ffb
vandeurenglenn <vandeurenglenn@gmail.com>
2023-05-21 20:11:43 +0200

    remove color-thief

--- src/doc-view.ts
+++ src/doc-view.ts
@@ -1,10 +1,10 @@
 import { LitElement, html, css } from 'lit';
 import Showdown from 'showdown'
 import showdownHighlight from 'showdown-highlight';
-import ColorThief from 'colorthief'
+// import ColorThief from 'colorthief'
 import { property } from 'lit/decorators.js';
 
-const colorThief = new ColorThief();
+// const colorThief = new ColorThief();
 // import manifest from './manifest.js';
 const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => {
   const hex = x.toString(16)
@@ -19,18 +19,18 @@ export class DocView extends LitElement {
     this.renderDoc(value)
   }
 
-  stealColors(img) {
-    const colors = colorThief.getColor(img);
-    const main = rgbToHex(colors[0] > 5 ? colors[0] - 5 : colors[0] , colors[1] > 5 ? colors[1] - 5 : colors[1] , colors[2]);
-    let secondary = rgbToHex(colors[0], colors[1], colors[2]);
+  // stealColors(img) {
+  //   const colors = colorThief.getColor(img);
+  //   const main = rgbToHex(colors[0] > 5 ? colors[0] - 5 : colors[0] , colors[1] > 5 ? colors[1] - 5 : colors[1] , colors[2]);
+  //   let secondary = rgbToHex(colors[0], colors[1], colors[2]);
       
-    secondary = `${secondary}78`;
-    document.body.style.setProperty('--md-sys-color-surface', secondary)
-    this.style.setProperty('--md-sys-color-surface', secondary)
+  //   secondary = `${secondary}78`;
+  //   document.body.style.setProperty('--md-sys-color-surface', secondary)
+  //   this.style.setProperty('--md-sys-color-surface', secondary)
 
-    document.body.style.setProperty('--md-sys-color-primary', main)
-    this.style.setProperty('--md-sys-color-primary', main)
-  }
+  //   document.body.style.setProperty('--md-sys-color-primary', main)
+  //   this.style.setProperty('--md-sys-color-primary', main)
+  // }
 
   async renderDoc(value) {
     await this.updateComplete
@@ -41,14 +41,14 @@ export class DocView extends LitElement {
     this.innerHTML = md
     const img = this.querySelector('img');
 
-    if (img.complete) {
-      this.stealColors(img)
+    // if (img.complete) {
+    //   this.stealColors(img)
       
-    } else {
-      img.addEventListener('load', () => {
-        this.stealColors(img)
-      });
-    }
+    // } else {
+    //   img.addEventListener('load', () => {
+    //     this.stealColors(img)
+    //   });
+    // }
   }
   constructor() {
     super()
